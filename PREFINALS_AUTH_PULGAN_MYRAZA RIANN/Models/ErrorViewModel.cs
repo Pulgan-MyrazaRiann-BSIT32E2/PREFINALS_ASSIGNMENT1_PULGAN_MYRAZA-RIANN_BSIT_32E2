bff66e8f7e0f4374bdd0e4e063760911631d85c5
Pulgan-MyrazaRiann-BSIT32E2 <81751499+Pulgan-MyrazaRiann-BSIT32E2@users.noreply.github.com>
2024-06-30 19:42:08 +0800

    Add files via upload

---
+++ PREFINALS_AUTH_PULGAN_MYRAZA RIANN/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PREFINALS_AUTH_PULGAN_MYRAZA_RIANN.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
